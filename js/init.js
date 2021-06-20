9803dff805e736fe7102f7a3dfcb1293da8b9780
comunicatell17 <comunicatell17@gmail.com>
2021-06-20 13:24:19 -0500

    mejoras

--- js/init.js
+++ js/init.js
@@ -3,10 +3,10 @@
 // @ts-ignore
 firebase.initializeApp({
   apiKey: "AIzaSyAmlJBEEIIqj414g4OQKYAjKppIv8hi1bw",
-    authDomain: "fbcomunica17.firebaseapp.com",
-    projectId: "fbcomunica17",
-    storageBucket: "fbcomunica17.appspot.com",
-    messagingSenderId: "341045309237",
-    appId: "1:341045309237:web:e0dbb791f0e804d69ca67c",
-    measurementId: "G-9EFQ3P64R2"
+  authDomain: "fbcomunica17.firebaseapp.com",
+  projectId: "fbcomunica17",
+  storageBucket: "fbcomunica17.appspot.com",
+  messagingSenderId: "341045309237",
+  appId: "1:341045309237:web:e0dbb791f0e804d69ca67c",
+  measurementId: "G-9EFQ3P64R2"
 });
